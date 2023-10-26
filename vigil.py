bf5190b3a2fcaf3a9c9ca03d0b9a8840ea04eab5
IDS-JASONL-PF3D\BOIDS 79 <jason.lionardi@binus.ac.id>
2023-10-26 15:15:26 +0700

    Add CWE and Publish Date in result, set request timeout

--- vigil.py
+++ vigil.py
@@ -1,5 +1,4 @@
 import argparse
-import keyword
 import sys
 from urllib import response
 import requests
@@ -18,8 +17,8 @@ parser.add_argument('-c', '--code', help='Mencari laporan berdasarkan kode CVE',
 #                    default=max,
 #                    help='sum the integers (default: find the max)')
 parser.add_argument('-y', '--year', help='Tahun versi OS, Aplikasi, atau kode CVE')
-parser.add_argument('-p', '--platform', help='Platform mobile yang ingin dicari', choices=['android', 'ios'],
-                    dest='platform')
+parser.add_argument('-p', '--platform', help='Platform mobile yang ingin dicari (default = android)', choices=['android', 'ios', 'iphone_os', 'ipad_os'],
+                    dest='virtualMatchString', default='android')
 # parser.add_argument('-l', '--list', help='List', action='store_const', const=True, default=False)
 # parser.add_argument('-s', '--source', help='API yang dijadikan sumber data', dest='api_source')
 parser.add_argument('-k', '--keyword', help='cari dengan kata kunci tertentu', dest='keywordSearch')
@@ -31,8 +30,11 @@ if len(sys.argv) == 1:
 
 # Parse args dan buat iterable
 all_args = vars(parser.parse_args())
-
 base_api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+
+# penyesuaian untuk platform
+# ios -> iphone_os dan ipad_os
+all_args['virtualMatchString'] = 'cpe:2.3:a:*:*:*:*:*:*:*:' + all_args['virtualMatchString'] + ':*:*'
 
 # Ambil args yang sudah diinput saja dan membangun url berisi query ke API
 input_args = {}
@@ -44,17 +46,22 @@ for key in all_args:
         # print(base_api_url)
         first_arg_passed = 1
 
+# Search date
+if all_args['year'] != None:
+    print('year tidak sama dengan None')
+    print(all_args['year'])
+    base_api_url = base_api_url + '&pubStartDate=' + all_args['year'] + '-01-01T00:00:00.000&pubEndDate=' + all_args['year'] + '-12-31T00:00:00.000'
+
 # print(base_api_url)
 
 # Menembak API sekaligus mengecek apakah terhubung jaringan/internet
 try:
-    response = requests.get(base_api_url)
+    response = requests.get(base_api_url, timeout=10)
 except:
-    print("Tidak terhubung jaringan.")
+    print("Terdapat kendala jaringan, mohon coba lagi.")
     sys.exit(1)
 
 # Menilai response API
-data = []
 if (response.status_code == 404):
     print('(404) CVE tidak ditemukan.')
     sys.exit(1)
@@ -63,30 +70,36 @@ else:
     if json_response['vulnerabilities'] == []:
         print('Tidak ada hasil.')
     else:
+        data = []
         i = 0
         t = Texttable()
-        t.add_row(['', 'CVE ID', 'Description'])
+        t.add_row(['', 'CVE ID', 'Description', 'Publish Date', 'CWE ID'])
         # t.add_row(['1', 'CVE-2023-3840', 'A vulnerability in the HPE Aruba Networking Virtual Intranet\xa0Access (VIA) client could allow malicious users to overwrite\xa0arbitrary files as NT AUTHORITY\\SYSTEM. A successful\xa0exploit could allow these malicious users to create a\xa0Denial-of-Service (DoS) condition affecting the Microsoft\xa0Windows operating System boot process.'])
         cve_details = json_response['vulnerabilities']
         for cve in cve_details:
             t.add_row([
                 i,
                 cve['cve']['id'],
-                cve['cve']['descriptions'][0]['value']
+                cve['cve']['descriptions'][0]['value'],
+                cve['cve']['published'],
+                cve['cve']['weaknesses'][0]['description'][0]['value'] if 'weaknesses' in cve['cve'] else '-'
             ])
 
             i+=1
 
             data.append({
                 'CVE': cve['cve']['id'],
-                'Description': cve['cve']['descriptions'][0]['value']
+                'Description': cve['cve']['descriptions'][0]['value'],
+                'Publish Date': cve['cve']['published'],
+                'CWE': cve['cve']['weaknesses'][0]['description'][0]['value'] if 'weaknesses' in cve['cve'] else '-'
             })
-            print(json.dumps(cve['cve']['id'], indent=1, sort_keys=True))
+            # print(json.dumps(cve['cve']['id'], indent=1, sort_keys=True))
+        print(t.draw())
+        
 
 # Bikin table
 # print(tabulate(data, ))
-print(t.draw())
-print(data[0])
+# print(data[0])
 
 sys.exit(0)
 
